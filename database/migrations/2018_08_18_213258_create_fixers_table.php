206177dce10407e211a7cb3616010e59bcc7c718
unknown <hesham14yahia@gmail.com>
2018-08-18 23:38:59 +0200

    Add Fixer Model & Migration

---
+++ database/migrations/2018_08_18_213258_create_fixers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFixersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fixers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('image_bath');
+            $table->date('birth_date');
+            $table->integer('city_id');
+            $table->integer('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fixers');
+    }
+}
