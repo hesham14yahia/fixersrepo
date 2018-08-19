488cc931fb70ef64dbc80e55e8e2e385e744a97b
unknown <hesham14yahia@gmail.com>
2018-08-19 12:11:35 +0200

    Finish testing fixer category

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/', function () {
 Route::get('/insert', function(){
     $fixer = Fixer::create(['name'=>'Fatama', 'image_bath'=> 'fatama.png', 'birth_date'=> '2010-10-5', 'city_id'=> '1', 'category_id' => '2']);
 });
+
+Route::get('/read', function(){
+    $fixer = Fixer::find(1);
+    $cat = Category::find($fixer->category_id);
+    echo $cat->name;
+});
